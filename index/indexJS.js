877e11025261de631076aee5b47b8e7f3896d7e0
BREEZNING <cfhcfh1122@gmail.com>
2018-12-09 14:27:01 +0800

    'learning'

---
+++ index/indexJS.js
@@ -0,0 +1,112 @@
+        function selection1(obj){     
+		 	var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=1;i<=5;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                	divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        } 
+        function selection2(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=6;i<=10;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        } 
+        function selection3(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=11;i<=15;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        } 
+        function selection4(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=16;i<=20;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        } 
+        function selection5(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=21;i<=25;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        }
+        function selection6(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=26;i<=30;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        }  
+        function selection7(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=31;i<=35;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        }  
+        function selection8(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=36;i<=40;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        }  
+         function selection9(obj){     
+            var _selectedvalue = obj.value; 
+            if(_selectedvalue==''){ _selectedvalue = '-1';} 
+            for(i=41;i<=45;i++){  
+                var divobj = document.getElementById('tb'+i);   
+                if(parseInt(_selectedvalue)==i){       
+                    divobj.style.display = 'block';}
+                else{
+                    divobj.style.display='none';
+                }
+            } 
+        }
+
+        function show(){
+            document.getElementById("lead").style.display="block";
+        }  
